5a3afd1c59913a60568073f61079fec78275c15d
PGMP1992 <pedro@dkltd.net>
2024-02-09 13:42:09 +0100

    Finished Week 6 Mini Project

--- Program.cs
+++ Program.cs
@@ -1,26 +1,23 @@
 ﻿/* Mini Project Wee6 
  * Author : Pedro Martinez
- * Date : 06/02/2023
+ * Date : 09/02/2023
  */
 
 using MiniProject;
-using System.ComponentModel.Design;
 
 bool validPrice = false;
+bool exit = false;
 
 string
     sCat = "",
     sProd = "",
-    sPrice = "";
+    sPrice = "",
+    sImput = "";
 
-int c_index = 1;  //Counter for Category Id 
 int p_index = 1;  //Counter for Category Id 
 
 double price = 0.00,
        sum = 00;
-
-//Category cat1 = new Category(0, "");
-//Product prod1 = new Product(0, "", 0.00);    
 
 Category cat1 = new Category("");
 Product prod1 = new Product("", "", 0.00);
@@ -28,83 +25,168 @@ Product prod1 = new Product("", "", 0.00);
 List<Product> list_Product = new List<Product>();
 List<Category> list_Category = new List<Category>();
 
-//
-while (true)
+while( !exit) // Loop for new entry after list shown
 {
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("To Enter a new product - Follow the Steps | To quit Enter + Q");
-    Console.ResetColor();
+    MsgColor("To Enter a new product - Follow the Steps | To quit Enter + Q", "Blue");
 
     //  Enter Category
-    
-    Console.Write("Enter a Category : ");
-    sCat = Console.ReadLine().Trim().ToUpper();
+    while (!exit)
+    {
+        Console.Write("Enter a Category : ");
+        sCat = Console.ReadLine().Trim().ToUpper();
 
-    if (sCat == "Q")
-    {
-        break;
-    }
-    else
-    {
-        // Check if doesnt exists 
-        if ( ! list_Category.Any(c => c.Name == sCat))
+        if (sCat == "Q")
         {
-            // Add New Category
-            list_Category.Add(new Category(sCat));
+            exit = true;
+            break;
+        }
+        else if (sCat.Length == 0)
+        {
+             MsgInvalidEntry();
         }
 
         else
         {
-            // Find Category 
-            if (list_Category.Where(p => p.Name.Contains(sCat)).Count() > 0)
+            // Check if Product exists 
+            if ( !list_Category.Any(c => c.Name == sCat))
             {
-                cat1.Name = list_Category.Where(p => p.Name.Contains(sCat)).ToString();
-                //list_Category.Add(new Category(sCat));
+                // Add New Category
+                list_Category.Add(new Category(sCat));
             }
-        }         
-        
-        // Products 
 
-        Console.Write("Enter a Product : ");
-        sProd = Console.ReadLine().Trim();
-        validPrice = true;
-
-            Console.Write("Enter Price : ");
-            sPrice = Console.ReadLine().Trim();
-
-            try
+            else
             {
-                price = Double.Parse(sPrice);
+                // Find Category 
+                if (list_Category.Where(p => p.Name.Contains(sCat)).Count() > 0)
+                {
+                    cat1.Name = list_Category.Where(p => p.Name.Contains(sCat)).ToString();
+                }
             }
-            catch (Exception ex)
+
+            // Enter Products 
+            sProd = " "; //reset string 
+            while (true)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(" Invalid Entry");
-                Console.ResetColor();
-                validPrice = false;
+                Console.Write("Enter a Product  : ");
+                sProd = Console.ReadLine().Trim().ToUpper();
+            
+                if (sProd.Length == 0 )
+                {
+                    MsgInvalidEntry();
+                }
+                else { 
+                    break; 
+                }
             }
-            finally
-            { }
+
+            validPrice = false; // reset validPrice
+            while (!validPrice)
+            {
+                Console.Write("Enter Price      : ");
+                sPrice = Console.ReadLine().Trim();
+
+                try
+                {
+                    price = double.Parse(sPrice);
+                    validPrice = true;
+                }
+                catch (Exception ex)
+                {
+                    MsgInvalidEntry(); 
+                }
+                finally // do nothing 
+                {
+                }
+
+            }  // while ( ! validPrice)
 
             p_index++;
             list_Product.Add(new Product(sCat, sProd, price));
 
+            MsgColor(" Product succefully Added ! ", "Yellow");
+            Console.WriteLine(" -----------------------------------------");
+            exit = false;
+
+        } // if else 
+        
+    } // while ! exit
+
+    // List is not empty show list
+    if (list_Category.Count() > 0)
+    {
+        PrintList(sum, list_Product);
+  
+        Console.WriteLine(" -----------------------------------------");
+        MsgColor(" To Enter a Product enter: P  | To quit Enter Q ", "Green");
+        
+        sImput = ""; // Reset Imput
+
+        while (sImput != "Q" && sImput != "P")
+        {
+            sImput = Console.ReadLine().Trim().ToUpper();
+            if (sImput == "P") //New Product
+            {
+                exit = false;
+            } 
+            else if (sImput == "Q") // Quit 
+            {
+                exit = true;
+            } else
+            {
+                MsgInvalidEntry();
+                exit = false;
+            }
+        }
+           
+    }
+
+} 
+
+
+// ==============================================================================
+
+
+// Color text in Prompt 
+static void MsgColor(string msg, string color)
+{
+    //Console.WriteLine(fname + " is " + age);
+    switch (color)
+    {
+        case "Yellow":
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(" Product succefully Added ! ");
-            Console.ResetColor();
+            break;
+        case "Red":
+            Console.ForegroundColor = ConsoleColor.Red;
+            break;
+        case "Green":
+            Console.ForegroundColor = ConsoleColor.Green;
+            break;
+        case "Blue":
+            Console.ForegroundColor = ConsoleColor.Blue;
+            break;
+    }
+    Console.WriteLine(msg); 
+    Console.ResetColor();
+}
 
-            Console.WriteLine(" -----------------------------------------" + c_index);
-        } // ifelse 
+//  Show error message 
+static void MsgInvalidEntry()
+{
+    MsgColor("Invalid Entry! Please try again: ", "Red");
+}
+    
 
-    } // While Loop not Q 
-
-    // Sort List to new List 
+// Print sorted List of products by Price
+static void PrintList(double sum, List<Product> list_Product)
+{
+    // Sort List by Price 
     List<Product> sortedByPrice = list_Product.OrderBy(list_Product => list_Product.Price).ToList();
 
-// Print lists with Sum of Prices
-Console.WriteLine("Sorted by Price");
-Console.WriteLine("===============");
-Console.WriteLine("Category".PadRight(10) + "Product".PadRight(10) + " Price");
+    // Print lists with Sum of Prices
+    Console.WriteLine("");
+    Console.WriteLine("Sorted by Price");
+    Console.WriteLine("===============");
+    MsgColor("Category".PadRight(10) + "Product".PadRight(10) + " Price", "Blue");
 
     foreach (Product i in sortedByPrice)
     {
@@ -116,5 +198,4 @@ Console.WriteLine("Category".PadRight(10) + "Product".PadRight(10) + " Price");
 
     Console.WriteLine("");
     Console.WriteLine("Total Amout :".PadRight(21) + sum);
-    Console.ReadLine();
-
+}
